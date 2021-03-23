42d9b20b075ffd780824263260fe467483d0e8cf
pushpendra Yadav <pushpendra.yadav21@outlook.com>
2021-03-23 12:24:23 +0530

    modified add product

--- src/app/common/product.ts
+++ src/app/common/product.ts
@@ -12,6 +12,7 @@ export class Product {
     lastUpdated: Date;
     productCategory:string;
     categoryId:number;
+    categoryName:string;
 
 }
 
--- src/app/components/create-product/create-product.component.ts
+++ src/app/components/create-product/create-product.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Product } from 'src/app/common/product';
+import { ProductCategory } from 'src/app/common/product-category';
+import { CategoryService } from 'src/app/services/category.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -9,26 +12,56 @@ import { ProductService } from 'src/app/services/product.service';
 })
 export class CreateProductComponent implements OnInit {
 
-  product:Product = new Product();
-  constructor(private productService:ProductService) { }
+  product: Product = new Product();
 
+  constructor(private productService: ProductService, private router: Router, private categoryService: CategoryService) { }
+
+  categoryList: ProductCategory[] = [];
   ngOnInit(): void {
-    
+    this.getProductCategoryList();
   }
 
-  onSubmit(){
+  onSubmit() {
     console.log(`${JSON.stringify(this.product)}`);
     this.saveProduct();
   }
 
-  saveProduct(){
-    this.product.isActive = true;
-    this.product.categoryId = 11;
+  saveProduct() {
+    let categoryId: number = this.getCategoryId(this.product.categoryName);
+    console.log('categoryId: ' + categoryId);
+    this.product.categoryId = categoryId;
+    console.log(JSON.stringify(this.product))
     this.productService.save(this.product).subscribe(
-      data =>{
-        alert("product added successfully");
-      }
+       data =>{
+         alert("product added successfully");
+         this.router.navigateByUrl('/productList')
+       }
     );
   }
 
+  getProductCategoryList() {
+    this.categoryService.getProductCategories().subscribe(
+      data => {
+        this.categoryList = data;
+        console.log(data)
+      }
+    )
+  }
+
+
+  getCategoryId(categoryName: string): number {
+    console.log('Category Name: ' + categoryName);
+    let id: number;
+    for (var tempCategory of this.categoryList) {
+
+      if (categoryName == tempCategory.categoryName) {
+        id = tempCategory.id;
+        break;
+      }
+    }
+    return id;
+  }
+
 }
+
+
